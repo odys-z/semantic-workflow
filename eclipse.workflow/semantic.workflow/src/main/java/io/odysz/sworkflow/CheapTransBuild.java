d91778f102045b698fee9fcbf7b6168b67b6d5cf
odysz <odysseusj@163.com>
2019-05-01 23:43:14 +0800

    0

--- eclipse.workflow/semantic.workflow/src/main/java/io/odysz/sworkflow/CheapTransBuild.java
+++ eclipse.workflow/semantic.workflow/src/main/java/io/odysz/sworkflow/CheapTransBuild.java
@@ -16,25 +16,21 @@ public class CheapTransBuild extends DATranscxt {
 	@Override
 	public Query select(String tabl, String... alias) {
 		Query q = super.select(tabl, alias);
-		q.doneOp((conn, sqls) -> Connects.select(conn, sqls.get(0)));
+		q.doneOp((conn, sqls) -> Connects.select(conn.connId(), sqls.get(0)));
 		return q;
 	}
 	
 	public Insert insert(String tabl, IUser usr) {
 		Insert i = super.insert(tabl);
-		i.doneOp((conn, sqls) -> Connects.commit(conn, usr, sqls));
+		i.doneOp((conn, sqls) -> Connects.commit(conn.connId(), usr, sqls));
 		return i;
 	}
 	
 	public Update update(String tabl, IUser usr) {
 		Update u = super.update(tabl);
-		u.doneOp((conn, sqls) -> Connects.commit(conn, usr, sqls));
+		u.doneOp((conn, sqls) -> Connects.commit(conn.connId(), usr, sqls));
 		return u;
 	}
-
-//	public CheapTransBuild(ISemantext semantext) {
-//		super(semantext);
-//	}
 
 	/**Build transact builder, initialize semantics in xtabl.
 	 * @param connId
